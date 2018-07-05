3f3d1f8ec79a848f40035d32292397b459142a33
Ngan Pham <40241719+npham25@users.noreply.github.com>
2018-07-05 01:56:57 -0500

    Add files via upload

---
+++ HW/FebCalendar.java
@@ -0,0 +1,49 @@
+import java.util.Scanner;
+
+public class FebCalendar {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.println("Enter the day that Jan.1 fell on: (0 = Sun., 1 = Mon., etc. ");
+		int n = input.nextInt();
+		
+		while (n < 0 || n > 6){
+			System.out.println("This is not a valid input. Enter 0-6.");
+			System.out.println("Enter the day that Jan.1 fell on: (0 = Sun., 1 = Mon., etc. ");
+			n = input.nextInt();
+		}
+
+		final int NUM_OF_DAY_PER_LINE = 7; // print 7 days per line
+		int day = 1; //count days
+		int First_Feb =0;
+		int blank = 0;
+
+		System.out.println ("Feburary");
+		System.out.println ("--------");
+		System.out.printf("%3s\t%3s\t%3s\t%3s\t%3s\t%3s\t%3s\n", "S", "M", "T", "W", "T", "F", "S"); //print tilte of the day
+
+		if (n <= 3){ // the day of Feb 1st, shift 3 days form Jan 1st
+			First_Feb = n + 3;
+		}
+		else{
+			First_Feb = n - 4;
+		}
+				
+		while (blank < First_Feb) {	
+			System.out.printf("\t");
+			blank ++;
+		}
+		int line = day +blank;
+		while (day <= 28){ //28 days in Feburary
+			System.out.printf("%3d\t",day);
+			day ++;
+			if (line%NUM_OF_DAY_PER_LINE == 0){ 
+			System.out.printf("\n");
+			}
+			line ++;
+		}		
+		
+		System.out.println();
+	
+	}
+}
