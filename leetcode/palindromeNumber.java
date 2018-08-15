511f3292ad114737ae3484d5a2915f7e75213c7d
Ngan Pham <40241719+npham25@users.noreply.github.com>
2018-08-15 10:40:23 -0500

    Update palindromeNumber.java

--- leetcode/palindromeNumber.java
+++ leetcode/palindromeNumber.java
@@ -1,22 +1,25 @@
-class Solution(object):
-    def isPalindrome(self, x):
-        """
-        :type x: int
-        :rtype: bool
-        """
-        if x < 0:
-            return False
+class Solution {
+    public boolean isPalindrome(int x) {
+        //d: number of digits in x - 1 (no count the first digit)
+        if (x < 0)
+            return false;
+
+        int d = 1;
+        while ( x / d >= 10) 
+            d *= 10;
+      
+        //continue loop until reach the middle digit
+        while (x != 0) {
+            int last = x % 10; //last digit of x
+            int first = x / d; // first digit of x
+            if (first != last)
+                return false;
+            else {
+                x = (x%d) / 10; // delete the first and last from x -> create new x
+                d = d / 100; //-> no more first, last 
+            }
+        }
+        return true;
         
-        d = 1
-        while x / d >= 10:
-            d = d * 10
-        
-        while x != 0:
-            if x/d != x%10:
-                return False
-            x = (x % d) / 10
-            d = d / 100
-        
-        return True
-        
-    
+    }
+}
